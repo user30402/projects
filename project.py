b56648444637cff7962eecbfc08f6b4c389033fa
user30402 <146691687+user30402@users.noreply.github.com>
2023-11-19 12:46:50 +0400

    SPTS Stats Calculator v1.3

--- project.py
+++ project.py
@@ -11,78 +11,136 @@ while 1 == 1:
     print()
     deco()
     print()
-    mult = input('Укажите величину статы (K, M, B, T, Qa, Qi): ')
+    print('1 - Калькулятор статов.')
     print()
-    getstat = float(input('Укажите кол-во статы за тик: '))
+    choice = input('Введите пункт: ')
     print()
-    delay = float(input('Укажите промежуток между получением статы в секундах (1.5 или 1): '))
-    print()
-    timemult = input('Укажите множитель времени для фарма (М, Ч, Д): ')
-    print()
-    time = float(input('Укажите время для афк: '))
-    print()
-    deco()
-    print()
+    if choice == '1':
+        mult = input('Укажите величину статы (K, M, B, T, Qa, Qi, Sx, Sp, Oc, No, De, Ud, Dd, Td): ')
+        print()
+        getstat = float(input('Укажите кол-во статы за тик: '))
+        print()
+        delay = float(input('Укажите промежуток между получением статы в секундах (0.5, 1 или 1.5): '))
+        print()
+        timemult = input('Укажите множитель времени для фарма (М, Ч, Д): ')
+        print()
+        time = float(input('Укажите время для афк: '))
+        print()
+        deco()
+        print()
 
-    if timemult == 'М' or timemult == 'м' or timemult == 'M' or timemult == 'm':
-        time *= 60
-    elif timemult == 'Ч' or timemult == 'ч' or timemult == 'H' or timemult == 'h':
-        time *= 3600
-    elif timemult == 'Д' or timemult == 'д' or timemult == 'D' or timemult == 'd':
-        time *= 86400
+        if timemult == 'М' or timemult == 'м' or timemult == 'M' or timemult == 'm':
+            time *= 60
+        elif timemult == 'Ч' or timemult == 'ч' or timemult == 'H' or timemult == 'h':
+            time *= (60 * 60)
+        elif timemult == 'Д' or timemult == 'д' or timemult == 'D' or timemult == 'd':
+            time *= (60 * 60 * 24)
 
-    if delay == 1.5:
-        time *= 0.80
+        if delay == 1.5:
+            time *= 0.80
+        elif delay == 0.5:
+            time *= 2
 
-    if mult == 'K' or mult == 'k' or mult == 'к' or mult == 'К':
-        getstat *= 1000
-        res = 1000
-        resmult = 1
-    elif mult == 'M' or mult == 'm' or mult == 'М' or mult == 'м':
-        getstat *= 1000000
-        res = 1000000
-        resmult = 2
-    elif mult == 'B' or mult == 'b' or mult == 'В' or mult == 'в':
-        getstat *= 1000000000
-        res = 1000000000
-        resmult = 3
-    elif mult == 'T' or mult == 't' or mult == 'Т' or mult == 'т':
-        getstat *= 1000000000000
-        res = 1000000000000
-        resmult = 4
-    elif mult == 'Qa' or mult == 'qA' or mult == 'QA' or mult == 'qa':
-        getstat *= 1000000000000000
-        res = 1000000000000000
-        resmult = 5
-    elif mult == 'Qi' or mult == 'qI' or mult == 'QI' or mult == 'qi':
-        getstat *= 1000000000000000000
-        res = 1000000000000000000
-        resmult = 6
+        if mult == 'K' or mult == 'k' or mult == 'к' or mult == 'К':
+            getstat *= 1000
+            res = 1000
+            resmult = 1
+        elif mult == 'M' or mult == 'm' or mult == 'М' or mult == 'м':
+            getstat *= 1000000
+            res = 1000000
+            resmult = 2
+        elif mult == 'B' or mult == 'b' or mult == 'В' or mult == 'в':
+            getstat *= 1000000000
+            res = 1000000000
+            resmult = 3
+        elif mult == 'T' or mult == 't' or mult == 'Т' or mult == 'т':
+            getstat *= 1000000000000
+            res = 1000000000000
+            resmult = 4
+        elif mult == 'Qa' or mult == 'qA' or mult == 'QA' or mult == 'qa':
+            getstat *= 1000000000000000
+            res = 1000000000000000
+            resmult = 5
+        elif mult == 'Qi' or mult == 'qI' or mult == 'QI' or mult == 'qi':
+            getstat *= 1000000000000000000
+            res = 1000000000000000000
+            resmult = 6
+        elif mult == 'Sx' or mult == 'sX' or mult == 'SX' or mult == 'sx':
+            getstat *= 1000000000000000000000
+            res = 1000000000000000000000
+            resmult = 7
+        elif mult == 'Sp' or mult == 'sP' or mult == 'SP' or mult == 'sp':
+            getstat *= 1000000000000000000000000
+            res = 1000000000000000000000000
+            resmult = 8
+        elif mult == 'Oc' or mult == 'oC' or mult == 'OC' or mult == 'oc':
+            getstat *= 1000000000000000000000000000
+            res = 1000000000000000000000000000
+            resmult = 9
+        elif mult == 'No' or mult == 'nO' or mult == 'NO' or mult == 'no':
+            getstat *= 1000000000000000000000000000000
+            res = 1000000000000000000000000000000
+            resmult = 10
+        elif mult == 'De' or mult == 'dE' or mult == 'DE' or mult == 'de':
+            getstat *= 1000000000000000000000000000000000
+            res = 1000000000000000000000000000000000
+            resmult = 11
+        elif mult == 'Ud' or mult == 'uD' or mult == 'UD' or mult == 'ud':
+            getstat *= 1000000000000000000000000000000000000
+            res = 1000000000000000000000000000000000000
+            resmult = 12
+        elif mult == 'Dd' or mult == 'dD' or mult == 'DD' or mult == 'dd':
+            getstat *= 1000000000000000000000000000000000000000
+            res = 1000000000000000000000000000000000000000
+            resmult = 13
+        elif mult == 'Td' or mult == 'tD' or mult == 'TD' or mult == 'td':
+            getstat *= 1000000000000000000000000000000000000000000
+            res = 1000000000000000000000000000000000000000000
+            resmult = 14
 
-    stat = getstat
-    stat *= time
-    stat /= res
+        stat = getstat
+        stat *= time
+        stat /= res
 
-    while stat > 1000:
-        stat /= 1000
-        resmult += 1
+        while stat > 1000:
+            stat /= 1000
+            resmult += 1
 
-    if resmult == 1:
-        mult = 'K'
-    elif resmult == 2:
-        mult = 'M'
-    elif resmult == 3:
-        mult = 'B'
-    elif resmult == 4:
-        mult = 'T'
-    elif resmult == 5:
-        mult = 'Qa'
-    elif resmult == 6:
-        mult = 'Qi'
-    
-    stat = round(stat, 2)
+        if resmult == 1:
+            mult = 'K'
+        elif resmult == 2:
+            mult = 'M'
+        elif resmult == 3:
+            mult = 'B'
+        elif resmult == 4:
+            mult = 'T'
+        elif resmult == 5:
+            mult = 'Qa'
+        elif resmult == 6:
+            mult = 'Qi'
+        elif resmult == 7:
+            mult = 'Sx'
+        elif resmult == 8:
+            mult = 'Sp'
+        elif resmult == 9:
+            mult = 'Oc'
+        elif resmult == 10:
+            mult = 'No'
+        elif resmult == 11:
+            mult = 'De'
+        elif resmult == 12:
+            mult = 'Ud'
+        elif resmult == 13:
+            mult = 'Dd'
+        elif resmult == 14:
+            mult = 'Td'
+        else:
+            mult = 'n/a'
+        
+        stat = round(stat, 2)
 
-    print(f'У вас будет +{stat}{mult} статы.')
+        print(f'У вас будет +{stat}{mult} статы.')
+
     print()
     deco()
     print()
